41caffa8e92b60d346e599e6352e3f559ab4fd1b
abhishekk <reach.abhishekj@gmail.com>
2016-08-15 09:41:56 -0500

    backend initial commit

---
+++ backend/user_api/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url, include
+from rest_framework import routers
+
+from . import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+
+urlpatterns = [
+    url(r'^', include(router.urls)),
+]
